1da0dd4316e4bcd0f7b15b53ac52eee64b37e4eb
725477 <116373882+725477@users.noreply.github.com>
2022-12-16 09:57:53 -0800

    Added a bird
    Added a bird and background and code. The code does not work for some reason.

---
+++ FlappyFinalAngelP2/Assets/Bird.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class Bird : MonoBehaviour { 
+
+    public float upForce = 200f;   
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    
+
+    // use this for initialization
+    Void Start()
+    {
+        rb2d = Getcomponent<RigidBody2D>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown (0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        isDead = true;
+       
+    }
+}
+        
+     
+
+
